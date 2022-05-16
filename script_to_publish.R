c554feec32fdbab2c7db0e9bd068e99432fd73bd
Benjamin Meyer <alaskasaurusrex.ak@gmail.com>
2022-05-15 20:23:26 -0800

    commit

---
+++ script_to_publish.R
@@ -0,0 +1,10 @@
+# SYNCH/PUBLISH BOOK TO BOOKDOWN.ORG (created this becuase the 'publish' button in corner of html interface window after render does not work)
+
+# clean out old versions
+bookdown::clean_book(TRUE)
+
+# render new version
+bookdown:: render_book()
+
+# publish book to https://bookdown.org/alaskasaurusrex_ak/kenai_juv_salmon_growth_analyses/
+bookdown::publish_book(name = "kenai_juv_salmon_growth_analyses", account = "alaskasaurusrex_ak")
